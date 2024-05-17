adcddb7d42f3520a9df498e5abae07ed22c4d7fd
Jon Wu <jonathan.wu124@gmail.com>
2024-05-17 19:59:45 -0400

    in the pokemoncardattacksdetails.jsx, added the variable to check if value is an array, then to add commans between each array element when displaying on the page

--- components/PokemonCardDetails/PokemonCardAttacksDetails.jsx
+++ components/PokemonCardDetails/PokemonCardAttacksDetails.jsx
@@ -1,10 +1,13 @@
 const PokemonCardAttacksDetails = (props) => {
+
+    const attackCostDisplay = Array.isArray(props.attackcost) ? props.attackcost.join(', ') : props.attackcost;
+
   return (
     <>
       <div>Attacks:</div>
       <p>Name: {props.attackname}</p>
       <p>{props.attacktext}</p>
-      <p>Cost: {props.attackcost}</p>
+      <p>Cost: {attackCostDisplay}</p>
       <p>Coverted Energy Cost: {props.attackcovertenergycost}</p>
       <p>Damage: {props.attackdamage}</p>
     </>
