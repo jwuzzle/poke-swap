612a9b7d19b27e8f450efbf607c4b3c632cc29a4
Jon Wu <jonathan.wu124@gmail.com>
2024-05-17 21:23:38 -0400

    initial code for pokeball small loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchHome from "../pages/SearchHome/SearchHome"
 import SearchResults from "../pages/SearchResults/SearchResults"
 import './App.scss'
+import PokeballLoader from "../components/PokeballLoader/PokeballLoader";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
         <Route path="/" element={<SearchHome />} />
         <Route path="/search/:name" element={<SearchResults />} />
+        <Route path="/loader" element={<PokeballLoader />} />
         </Routes>
       </BrowserRouter>
     </>
