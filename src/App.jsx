6f7a335d675d24e11c4a5288aadda2fd482649cf
Jon Wu <jonathan.wu124@gmail.com>
2024-05-20 16:45:45 -0400

    added setup and connection to cloudinary for hosting uploaded card images for posts. created the create card post page with the cloudinary connection.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import SearchResults from "../pages/SearchResults/SearchResults"
 import './App.scss'
 import PokeballLoader from "../components/PokeballLoader/PokeballLoader";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
+import PhotoUpload from "../components/PhotoUpload/PhotoUpload";
+import CreateCardPostPage from "../pages/CreateCardPostPage/CreateCardPostPage";
 
 function App() {
 
@@ -15,6 +17,7 @@ function App() {
         <Route path="/" element={<SearchHome />} />
         <Route path="/search/:name" element={<SearchResults />} />
         <Route path="/loader" element={<PokeballLoader />} />
+        <Route path="/search/:name/:id/upload" element={<CreateCardPostPage />} />
         </Routes>
       </BrowserRouter>
     </>
