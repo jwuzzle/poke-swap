8ee4fd03131f4f836d7be1d39cc548aaae8a1bff
Jon Wu <jonathan.wu124@gmail.com>
2024-05-30 12:08:48 -0400

    added the ability to click on a card in user's collection to take to the details page.

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/collection" element={<UserCardCollection />} />
         <Route path="/collection/:userid" element={<OtherUsersCardCollectionPage />} />
+        <Route path="/collection/:userid/listing/:postid" element={<CardPostDetailsPage />} />
 
         </Routes>
       </BrowserRouter>
